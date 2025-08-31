baa23a292147f9bdde6e6ce51efd189f9d070a96
prudhvi reddy <prudhvireddy.m01@gmail.com>
2025-08-31 10:51:37 +0530

    kottie with composition result

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -62,6 +62,7 @@ kotlin {
                 implementation(compose.foundation)
                 implementation(compose.material3)
                 implementation(compose.components.resources)
+                implementation(libs.coroutines.core)
             }
         }
 
